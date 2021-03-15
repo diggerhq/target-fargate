936114b5736ca1c1f0e464f5bd4bb36db1b3d7ea
motatoes <moe.habib9@gmail.com>
2021-03-16 01:02:03 +0300

    include module for service with no lb

--- module-fargate-service-nolb/main.tf
+++ module-fargate-service-nolb/main.tf
@@ -90,7 +90,7 @@ resource "aws_ecs_service" "app" {
   # propagate_tags          = "SERVICE"
 
   # workaround for https://github.com/hashicorp/terraform/issues/12634
-  depends_on = [aws_alb_listener.http]
+  depends_on = []
 
   # [after initial apply] don't override changes made to task_definition
   # from outside of terraform (i.e.; fargate cli)
