1de743538fa3a9675815524b7ce348c8c7ffd17b
motatoes <moe.habib9@gmail.com>
2021-03-18 00:32:06 +0300

    fix template syntax

--- main/service.template.tf
+++ main/service.template.tf
@@ -28,7 +28,7 @@
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
     {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
-    {% if task_memory%}task_memory = "{{task_memory}}" {% endif %}
+    {% if task_memory %}task_memory = "{{task_memory}}" {% endif %}
   }
 
   output "{{service_name}}_docker_registry" {
@@ -62,7 +62,7 @@
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
     {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
-    {% if task_memory%}task_memory = "{{task_memory}}" {% endif %}
+    {% if task_memory %}task_memory = "{{task_memory}}" {% endif %}
   }
 
   output "{{service_name}}_docker_registry" {
