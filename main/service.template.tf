a95192e0d7964bb9cd4134937ffb76dd11ad5e8a
motatoes <moe.habib9@gmail.com>
2021-03-18 00:19:37 +0300

    fix syntax

--- main/service.template.tf
+++ main/service.template.tf
@@ -27,8 +27,8 @@
     # ecs_autoscale_max_instances
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
-    {% if task_cpu %}task_cpu="{{task_cpu}}"{% endif %}
-    {% if task_memory%}task_memory="{{task_memory}}"{% endif %}
+    {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
+    {% if task_memory%}task_memory = "{{task_memory}}" {% endif %}
   }
 
   output "{{service_name}}_docker_registry" {
@@ -61,8 +61,8 @@
     # ecs_autoscale_max_instances
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
-    {% if task_cpu %}task_cpu="{{task_cpu}}"{% endif %}
-    {% if task_memory%}task_memory="{{task_memory}}"{% endif %}
+    {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
+    {% if task_memory%}task_memory = "{{task_memory}}" {% endif %}
   }
 
   output "{{service_name}}_docker_registry" {
