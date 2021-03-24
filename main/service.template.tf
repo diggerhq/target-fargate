cd203e426f6471918ba4cc9f2a25dee6cbfcc644
motatoes <moe.habib9@gmail.com>
2021-03-24 18:42:32 +0300

    remove auto SSL listener in ALB

--- main/service.template.tf
+++ main/service.template.tf
@@ -25,7 +25,7 @@
     launch_type = "{{launch_type}}"
     # ecs_autoscale_min_instances
     # ecs_autoscale_max_instances
-    lb_ssl_certificate_arn = "arn:aws:acm:eu-west-1:262499071169:certificate/411063e8-cd77-4498-921a-23adb15a1b9b"
+    # lb_ssl_certificate_arn = "arn:aws:acm:eu-west-1:262499071169:certificate/411063e8-cd77-4498-921a-23adb15a1b9b"
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
     {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
