d644baadcd8cbf9206d67d53c4bd51f206a72024
motatoes <moe.habib9@gmail.com>
2021-04-09 23:37:17 +0300

    api gateway integration

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -55,20 +55,20 @@ resource "aws_api_gateway_method_response" "main" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration" "main2" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = aws_api_gateway_method_response.main.status_code
+# resource "aws_api_gateway_integration" "main2" {
+#   rest_api_id = aws_api_gateway_rest_api.main.id
+#   resource_id = aws_api_gateway_resource.main.id
+#   http_method = aws_api_gateway_method.main.http_method
+#   status_code = aws_api_gateway_method_response.main.status_code
 
-  response_templates = {
-    "application/json" = ""
-  }
+#   response_templates = {
+#     "application/json" = ""
+#   }
 
-  depends_on = [
-    aws_api_gateway_integration.main
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_integration.main
+#   ]
+# }
 
 resource "aws_api_gateway_deployment" "main" {
   depends_on  = ["aws_api_gateway_integration.main"]
