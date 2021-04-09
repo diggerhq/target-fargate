58c0c7a943712d9e940e80a22df9b08d122f5a1b
motatoes <moe.habib9@gmail.com>
2021-04-09 23:24:38 +0300

    api gateway integration

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -55,7 +55,7 @@ resource "aws_api_gateway_method_response" "main" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_integrationration_response" "main" {
+resource "aws_api_gateway_integration" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   resource_id = aws_api_gateway_resource.main.id
   http_method = aws_api_gateway_method.main.http_method
