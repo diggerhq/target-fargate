60dc0a4c6f7112c2f7735dee0875ae5e944b3c0b
motatoes <moe.habib9@gmail.com>
2021-04-11 00:53:17 +0300

    update sg

--- fargate-service-module/securitygroup.tf
+++ fargate-service-module/securitygroup.tf
@@ -22,7 +22,7 @@ resource "aws_security_group_rule" "nsg_task_egress_rule" {
   security_group_id = aws_security_group.nsg_task.id
 }
 
-resource "aws_security_group_rule" "nsg_task_egress_rule" {
+resource "aws_security_group_rule" "nsg_task_igress_rule" {
   description = "Allows task to establish connections to all resources"
   type        = "ingress"
   from_port   = "443"
