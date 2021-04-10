2e1b8d6d9ab961f45baef2227ed667f74eaa998f
motatoes <moe.habib9@gmail.com>
2021-04-10 16:58:16 +0300

    privatelink sg

---
+++ main/vpc_privatelink.tf
@@ -0,0 +1,27 @@
+
+resource "aws_security_group" "vpc_endpoint_security" {
+  name_prefix = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["com.amazonaws.eu-west-1.ssm"]
+  }
+}
+
+# for fargate access to ssm secrets
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = "com.amazonaws.eu-west-1.ssm"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint_security.id,
+  ]
+
+  private_dns_enabled = true
+}
