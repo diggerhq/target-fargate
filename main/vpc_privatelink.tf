54bff56a7d283d726a25bd42c285daef999a0aff
motatoes <moe.habib9@gmail.com>
2021-04-10 19:29:55 +0300

    include s3 endpoint gateway access

--- main/vpc_privatelink.tf
+++ main/vpc_privatelink.tf
@@ -32,3 +32,10 @@ resource "aws_vpc_endpoint" "ssm_parameter_store" {
 
   private_dns_enabled = true
 }
+
+# Include the gateway endpoint to s3 (required by ssm)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.eu-west-1.s3"
+  vpc_endpoint_type = "Gateway"
+}
