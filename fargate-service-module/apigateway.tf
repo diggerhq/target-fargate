87cba6c01951b12e9ac495b715fb88fa026c4424
motatoes <moe.habib9@gmail.com>
2021-04-12 23:29:21 +0300

    include cors options for api gateway

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -40,14 +40,47 @@ resource "aws_api_gateway_method" "main" {
 }
 
 resource "aws_api_gateway_method" "options" {
-  rest_api_id      = aws_api_gateway_rest_api.main.id
-  resource_id      = aws_api_gateway_resource.main.id
-  http_method      = "OPTIONS"
-  authorization    = "NONE"
-  api_key_required = false
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
+    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+    resource_id   = "${aws_api_gateway_resource.main.id}"
+    http_method   = "OPTIONS"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+    resource_id   = "${aws_api_gateway_resource.main.id}"
+    http_method   = "${aws_api_gateway_method.options.http_method}"
+    status_code   = "200"
+    response_models {
+        "application/json" = "Empty"
+    }
+    response_parameters {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+    depends_on = ["aws_api_gateway_method.options"]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+    resource_id   = "${aws_api_gateway_resource.main.id}"
+    http_method   = "${aws_api_gateway_method.options.http_method}"
+    type          = "MOCK"
+    depends_on = ["aws_api_gateway_method.options"]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+    resource_id   = "${aws_api_gateway_resource.main.id}"
+    http_method   = "${aws_api_gateway_method.options.http_method}"
+    status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
+    depends_on = ["aws_api_gateway_method_response.options_200"]
 }
 
 resource "aws_api_gateway_integration" "main" {
