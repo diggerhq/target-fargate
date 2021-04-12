c933d0571dbf1a881a904da733d0316d67b94db1
motatoes <moe.habib9@gmail.com>
2021-04-12 23:52:05 +0300

    add equals sign

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -51,10 +51,10 @@ resource "aws_api_gateway_method_response" "options_200" {
     resource_id   = "${aws_api_gateway_resource.main.id}"
     http_method   = "${aws_api_gateway_method.options.http_method}"
     status_code   = "200"
-    response_models {
+    response_models = {
         "application/json" = "Empty"
     }
-    response_parameters {
+    response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = true,
         "method.response.header.Access-Control-Allow-Methods" = true,
         "method.response.header.Access-Control-Allow-Origin" = true
@@ -74,7 +74,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
     rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
     resource_id   = "${aws_api_gateway_resource.main.id}"
     http_method   = "${aws_api_gateway_method.options.http_method}"
-    status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
+  status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
     response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
