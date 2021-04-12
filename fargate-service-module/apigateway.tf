b0e27161c09cfd4978b54369c7b7acabab0f2098
motatoes <moe.habib9@gmail.com>
2021-04-12 19:41:04 +0300

    add integration response

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -75,20 +75,16 @@ resource "aws_api_gateway_method_response" "main" {
   status_code = "200"
 }
 
-# resource "aws_api_gateway_integration" "main2" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   resource_id = aws_api_gateway_resource.main.id
-#   http_method = aws_api_gateway_method.main.http_method
-#   status_code = aws_api_gateway_method_response.main.status_code
+resource "aws_api_gateway_integration_response" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.main.id
+  http_method = aws_api_gateway_method.main.http_method
+  status_code = aws_api_gateway_method_response.main.status_code
 
-#   response_templates = {
-#     "application/json" = ""
-#   }
-
-#   depends_on = [
-#     aws_api_gateway_integration.main
-#   ]
-# }
+  response_templates = {
+    "application/json" = ""
+  }
+}
 
 resource "aws_api_gateway_deployment" "main" {
   depends_on  = ["aws_api_gateway_integration.main"]
