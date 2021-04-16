c221f6159917b030e7547f6f0f0547bce34b2023
motatoes <moe.habib9@gmail.com>
2021-04-16 18:49:16 +0300

    cleanup for example

--- main/service.template.tf
+++ main/service.template.tf
@@ -1,32 +1,21 @@
 
 {% if load_balancer %}
   module "service-{{service_name}}" {
-    source = "git::https://github.com/diggerhq/module-fargate-service.git?ref=qc"
+    # make sure you version your modules and dendencies
+    source = "git::https://github.com/diggerhq/module-fargate-service.git?ref=v1.0.5"
 
     ecs_cluster = aws_ecs_cluster.app
     service_name = "{{service_name}}"
     region = var.region
     service_vpc = aws_vpc.vpc
     service_security_groups = [aws_security_group.ecs_service_sg.id]
-    # image_tag_mutability
     lb_subnet_a = aws_subnet.public_subnet_a
     lb_subnet_b = aws_subnet.public_subnet_b
-    # lb_port
-    # lb_protocol
     internal = false
-    # deregistration_delay
     health_check = "{{health_check}}"
-    # health_check_interval
-    # health_check_timeout
-    # health_check_matcher
-    # lb_access_logs_expiration_days
     container_port = "{{container_port}}"
-    # replicas
     container_name = "{{app_name}}-{{environment}}-{{service_name}}"
     launch_type = "{{launch_type}}"
-    # ecs_autoscale_min_instances
-    # ecs_autoscale_max_instances
-    # lb_ssl_certificate_arn = "arn:aws:acm:eu-west-1:262499071169:certificate/411063e8-cd77-4498-921a-23adb15a1b9b"
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
     {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
@@ -51,17 +40,11 @@
     region = var.region
     service_vpc = aws_vpc.vpc
     service_security_groups = [aws_security_group.ecs_service_sg.id]
-    # image_tag_mutability
     lb_subnet_a = aws_subnet.public_subnet_a
     lb_subnet_b = aws_subnet.public_subnet_b
-    # lb_port
-    # lb_protocol
     internal = false
-    # replicas
     container_name = "{{app_name}}-{{environment}}-{{service_name}}"
     launch_type = "{{launch_type}}"
-    # ecs_autoscale_min_instances
-    # ecs_autoscale_max_instances
     default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
     tags = var.tags
     {% if task_cpu %}
