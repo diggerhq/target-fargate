9ee14afb9d1589603ec44b1dd90c2f3b447d79ad
motatoes <moe.habib9@gmail.com>
2021-05-06 12:59:08 +0300

    cassandra and kafka

--- main/cassandra.template.tf
+++ main/cassandra.template.tf
@@ -1,1 +1,13 @@
 
+
+resource "aws_instance" "cassandra" {
+  count = 3
+  ami           = "ami-0960d43e248a0602c"
+  instance_type = "m5.large"
+  subnet_id = aws_subnet.public_subnet_a.id
+
+  ebs_block_device {
+    device_name = "main_device"
+    size = 120
+  }
+}
--- main/kafka.template.tf
+++ main/kafka.template.tf
@@ -1,1 +1,116 @@
 
+resource "aws_security_group" "msk_sg" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_kms_key" "kms" {
+  description = "msk kms key"
+}
+
+resource "aws_cloudwatch_log_group" "msk-cloudwatch" {
+  name = "msk_broker_logs"
+}
+
+resource "aws_s3_bucket" "kafka_bucket" {
+  bucket-prefix = "${var.app}-msk-broker-logs-${var.environment}"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehose_msk_role"
+
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Action": "sts:AssumeRole", 
+    "Principal": {
+      "Service": "firehose.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }
+  ]
+}
+EOF
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "msk_stream" {
+  name        = "terraform-kinesis-firehose-msk-broker-logs-stream"
+  destination = "s3"
+
+  s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.kafka_bucket.arn
+  }
+
+  tags = {
+    LogDeliveryEnabled = "placeholder"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags["LogDeliveryEnabled"],
+    ]
+  }
+}
+
+resource "aws_msk_cluster" "example" {
+  cluster_name           = "example"
+  kafka_version          = "2.8.0"
+  number_of_broker_nodes = 2
+
+  broker_node_group_info {
+    instance_type   = "kafka.m5.large"
+    ebs_volume_size = 1000
+    client_subnets = [
+      aws_subnet.public_subnet_a.id,
+      aws_subnet.public_subnet_b.id,
+    ]
+    security_groups = [aws_security_group.msk_sg.id]
+  }
+
+  encryption_info {
+    encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = true
+      }
+      node_exporter {
+        enabled_in_broker = true
+      }
+    }
+  }
+
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.msk-cloudwatch.name
+      }
+      firehose {
+        enabled         = true
+        delivery_stream = aws_kinesis_firehose_delivery_stream.msk_stream.name
+      }
+      s3 {
+        enabled = true
+        bucket  = aws_s3_bucket.kafka_bucket.id
+        prefix  = "logs/msk-"
+      }
+    }
+  }
+
+}
+
+output "DGVAR_zookeeper_connect_string" {
+  value = aws_msk_cluster.example.zookeeper_connect_string
+}
+
+output "DGVAR_bootstrap_brokers_tls" {
+  description = "TLS connection host:port pairs"
+  value       = aws_msk_cluster.example.bootstrap_brokers_tls
+}
