e64739cace6e6e36bdbfd65c62a85ae97dab9dd3
motatoes <moe.habib9@gmail.com>
2021-05-27 20:52:19 +0300

    remove gateway links

--- fargate-service-module/apigateway.tf
+++ fargate-service-module/apigateway.tf
@@ -27,107 +27,107 @@ resource "aws_api_gateway_authorizer" "cognito_auth" {
 }
 
 
-resource "aws_api_gateway_method" "main" {
-  rest_api_id      = aws_api_gateway_rest_api.main.id
-  resource_id      = aws_api_gateway_resource.main.id
-  http_method      = "ANY"
-  authorization    = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.cognito_auth.id
-  api_key_required = false
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
-}
+# resource "aws_api_gateway_method" "main" {
+#   rest_api_id      = aws_api_gateway_rest_api.main.id
+#   resource_id      = aws_api_gateway_resource.main.id
+#   http_method      = "ANY"
+#   authorization    = "COGNITO_USER_POOLS"
+#   authorizer_id = aws_api_gateway_authorizer.cognito_auth.id
+#   api_key_required = false
+#   request_parameters = {
+#     "method.request.path.proxy" = true
+#   }
+# }
 
-resource "aws_api_gateway_method" "options" {
-    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
-    resource_id   = "${aws_api_gateway_resource.main.id}"
-    http_method   = "OPTIONS"
-    authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "options" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+#     resource_id   = "${aws_api_gateway_resource.main.id}"
+#     http_method   = "OPTIONS"
+#     authorization = "NONE"
+# }
 
-resource "aws_api_gateway_method_response" "options_200" {
-    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
-    resource_id   = "${aws_api_gateway_resource.main.id}"
-    http_method   = "${aws_api_gateway_method.options.http_method}"
-    status_code   = "200"
-    response_models = {
-        "application/json" = "Empty"
-    }
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-    depends_on = ["aws_api_gateway_method.options"]
-}
+# resource "aws_api_gateway_method_response" "options_200" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+#     resource_id   = "${aws_api_gateway_resource.main.id}"
+#     http_method   = "${aws_api_gateway_method.options.http_method}"
+#     status_code   = "200"
+#     response_models = {
+#         "application/json" = "Empty"
+#     }
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = true,
+#         "method.response.header.Access-Control-Allow-Methods" = true,
+#         "method.response.header.Access-Control-Allow-Origin" = true
+#     }
+#     depends_on = ["aws_api_gateway_method.options"]
+# }
 
-resource "aws_api_gateway_integration" "options_integration" {
-    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
-    resource_id   = "${aws_api_gateway_resource.main.id}"
-    http_method   = "${aws_api_gateway_method.options.http_method}"
-    type          = "MOCK"
-    depends_on = ["aws_api_gateway_method.options"]
-}
+# resource "aws_api_gateway_integration" "options_integration" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+#     resource_id   = "${aws_api_gateway_resource.main.id}"
+#     http_method   = "${aws_api_gateway_method.options.http_method}"
+#     type          = "MOCK"
+#     depends_on = ["aws_api_gateway_method.options"]
+# }
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-    rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
-    resource_id   = "${aws_api_gateway_resource.main.id}"
-    http_method   = "${aws_api_gateway_method.options.http_method}"
-  status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-        "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    }
-    depends_on = ["aws_api_gateway_method_response.options_200"]
-}
+# resource "aws_api_gateway_integration_response" "options_integration_response" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.main.id}"
+#     resource_id   = "${aws_api_gateway_resource.main.id}"
+#     http_method   = "${aws_api_gateway_method.options.http_method}"
+#   status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+#         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+#         "method.response.header.Access-Control-Allow-Origin" = "'*'"
+#     }
+#     depends_on = ["aws_api_gateway_method_response.options_200"]
+# }
 
-resource "aws_api_gateway_integration" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
+# resource "aws_api_gateway_integration" "main" {
+#   rest_api_id = aws_api_gateway_rest_api.main.id
+#   resource_id = aws_api_gateway_resource.main.id
+#   http_method = aws_api_gateway_method.main.http_method
 
-  type                    = "HTTP_PROXY"
-  integration_http_method = "ANY"
-  uri                     = "http://${aws_lb.main.dns_name}/{proxy}"
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.main.id
-  timeout_milliseconds    = 29000 # 50-29000
+#   type                    = "HTTP_PROXY"
+#   integration_http_method = "ANY"
+#   uri                     = "http://${aws_lb.main.dns_name}/{proxy}"
+#   connection_type         = "VPC_LINK"
+#   connection_id           = aws_api_gateway_vpc_link.main.id
+#   timeout_milliseconds    = 29000 # 50-29000
 
-  cache_key_parameters = ["method.request.path.proxy"]
-  request_parameters = {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
-}
+#   cache_key_parameters = ["method.request.path.proxy"]
+#   request_parameters = {
+#     "integration.request.path.proxy" = "method.request.path.proxy"
+#   }
+# }
 
-resource "aws_api_gateway_method_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = "200"
-}
+# resource "aws_api_gateway_method_response" "main" {
+#   rest_api_id = aws_api_gateway_rest_api.main.id
+#   resource_id = aws_api_gateway_resource.main.id
+#   http_method = aws_api_gateway_method.main.http_method
+#   status_code = "200"
+# }
 
-resource "aws_api_gateway_integration_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = aws_api_gateway_method_response.main.status_code
+# resource "aws_api_gateway_integration_response" "main" {
+#   rest_api_id = aws_api_gateway_rest_api.main.id
+#   resource_id = aws_api_gateway_resource.main.id
+#   http_method = aws_api_gateway_method.main.http_method
+#   status_code = aws_api_gateway_method_response.main.status_code
 
-  response_templates = {
-    "application/json" = ""
-  }
+#   response_templates = {
+#     "application/json" = ""
+#   }
 
-  depends_on = [
-    aws_api_gateway_integration.main
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_integration.main
+#   ]
+# }
 
-resource "aws_api_gateway_deployment" "main" {
-  depends_on  = ["aws_api_gateway_integration.main"]
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  stage_name  = "v1"
-}
+# resource "aws_api_gateway_deployment" "main" {
+#   depends_on  = ["aws_api_gateway_integration.main"]
+#   rest_api_id = aws_api_gateway_rest_api.main.id
+#   stage_name  = "v1"
+# }
 
 
 data "aws_route53_zone" "main" {
