017b6bbf6d6bd0d0253d983678afa622bcd93fe4
motatoes <moe.habib9@gmail.com>
2021-05-28 21:32:28 +0300

    cw event stream for es

--- main/elasticsearch.template.tf
+++ main/elasticsearch.template.tf
@@ -19,16 +19,16 @@
     name_prefix = "${var.app}-${var.environment}-es-lambda-role"
 
     assume_role_policy = <<EOF
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {"Service": "lambda.amazonaws.com"},
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "lambda.amazonaws.com"},
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
     EOF
   }
 
