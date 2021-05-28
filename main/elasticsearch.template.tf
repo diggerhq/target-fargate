e7232bc031d12aa3dc2eaa4cddd649a6124dbc0e
motatoes <moe.habib9@gmail.com>
2021-05-28 21:12:31 +0300

    cw event stream for es

--- main/elasticsearch.template.tf
+++ main/elasticsearch.template.tf
@@ -23,6 +23,11 @@
         "Version": "2012-10-17",
         "Statement": [
             {
+                "Effect": "Allow",
+                "Principal": {"Service": "lambda.amazonaws.com"},
+                "Action": "sts:AssumeRole"
+            },
+            {
                 "Action": [
                     "es:*"
                 ],
