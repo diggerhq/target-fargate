44b7ac430780b925b5582ed86c6586f4332d0a2e
motatoes <moe.habib9@gmail.com>
2021-06-02 20:50:21 +0300

    remove duplicate output

--- module-fargate-service-nolb/securitygroup.tf
+++ module-fargate-service-nolb/securitygroup.tf
@@ -32,7 +32,3 @@ resource "aws_security_group_rule" "nsg_task_egress_rule" {
 
   security_group_id = aws_security_group.nsg_task.id
 }
-
-output "task_security_group_id" {
-  value = aws_security_group.nsg_task.id
-}
