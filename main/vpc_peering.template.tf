e517946c1143c7819a9cb92a9f5d5cf7d36f96e9
motatoes <moe.habib9@gmail.com>
2021-06-04 00:51:03 +0300

    autoo accept

--- main/vpc_peering.template.tf
+++ main/vpc_peering.template.tf
@@ -5,8 +5,8 @@
     # peer_owner_id = var.peer_owner_id
     peer_vpc_id   = "{{environment_config.peer_vpc}}"
     vpc_id        = aws_vpc.vpc.id
-    auto_accept   = true
-    
+    auto_accept   = false
+
     {% if environment_config.peer_vpc_region %}
     peer_region = "{{environment_config.peer_vpc_region}}"
     {% endif %}
@@ -19,6 +19,13 @@
     #   allow_remote_vpc_dns_resolution = true
     # }
   }
+
+
+  # Accepter's side of the connection.
+  resource "aws_vpc_peering_connection_accepter" "peer" {
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer_{{environment_config.peer_vpc}}.id
+    auto_accept               = true
+  }  
 
   resource "aws_route" "requestor_{{peer_vpc}}" {
     route_table_id = aws_route_table.route_table_public.id
