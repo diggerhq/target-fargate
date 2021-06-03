01998ff4b399355c3c1f4b846e2a4f9c493c4c39
motatoes <moe.habib9@gmail.com>
2021-06-04 00:17:50 +0300

    include route table entries

--- main/vpc_peering.template.tf
+++ main/vpc_peering.template.tf
@@ -1,14 +1,38 @@
 
-{% if environment_config.peer_vpcs %}
-  {% set peer_vpcs = environment_config.peer_vpcs.split(",") %}
+{% if environment_config.peer_vpc %}
+  # fetch data about the requester VPC
+  data "aws_vpc" "requester" {
+    id    = aws_vpc.vpc.id
+  }
 
-  {% for peer_vpc in peer_vpcs %}
+  data "aws_route_tables" "requester" {
+    vpc_id = aws_vpc.vpc.id
+  }
 
-    resource "aws_vpc_peering_connection" "peer_{{peer_vpc}}" {
-      # peer_owner_id = var.peer_owner_id
-      peer_vpc_id   = "{{peer_vpc}}"
-      vpc_id        = aws_vpc.vpc.id
+  resource "aws_vpc_peering_connection" "peer_{{peer_vpc}}" {
+    # peer_owner_id = var.peer_owner_id
+    peer_vpc_id   = "{{peer_vpc}}"
+    vpc_id        = aws_vpc.vpc.id
+
+    {% if environment_config.peer_vpc_region %}
+    peer_region = "{{environment_config.peer_vpc_region}}"
+    {% endif %}
+
+    accepter {
+      allow_remote_vpc_dns_resolution = true
     }
 
-  {% endfor %}
+    # requester {
+    #   allow_remote_vpc_dns_resolution = true
+    # }
+  }
+
+  resource "aws_route" "requestor_{{peer_vpc}}" {
+    count = length(data.aws_route_tables.requester.ids)
+    route_table_id = data.aws_route_tables.requester.ids[count.index]
+    destination_cidr_block = var.vpcCIDRblock
+    vpc_peering_cononection_id = aws_vpc_peering_connection.peer_{{peer_vpc}}.id
+  }
+
+
 {% endif %}
