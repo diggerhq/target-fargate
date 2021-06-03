4e7a6cd878907fd3c0ff2d51607d22cb5c2cc051
motatoes <moe.habib9@gmail.com>
2021-06-03 22:14:39 +0300

    set vpc peers

--- main/vpc_peering.tempate.tf
+++ main/vpc_peering.tempate.tf
@@ -1,8 +1,8 @@
 
 {% if environment_config.peer_subnets %}
-  {% set peer_subnets = environment_config.peer_subnets.split(';') %}
+  {% set peer_subnets = environment_config.peer_subnets.split(",") %}
 
   {% for subnet in peer_subnets %}
-    
+
   {% endfor %}
 {% endif %}
