e8a6ee9740f2c388aa1d2f19dd76a4fac7e10e12
motatoes <moe.habib9@gmail.com>
2021-06-03 22:23:21 +0300

    create peers

---
+++ main/vpc_peering.template.tf
@@ -0,0 +1,14 @@
+
+{% if environment_config.peer_subnets %}
+  {% set peer_vpcs = environment_config.peer_vpcs.split(",") %}
+
+  {% for peer_vpc in peer_vpcs %}
+
+    resource "aws_vpc_peering_connection" "peer_{{peer_vpc}}" {
+      # peer_owner_id = var.peer_owner_id
+      peer_vpc_id   = "{{peer_vpc}}"
+      vpc_id        = aws_vpc.vpc.id
+    }
+
+  {% endfor %}
+{% endif %}
