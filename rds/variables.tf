257d3ca380ab60fd10d77ab48742f8046a270c4c
motatoes <moe.habib9@gmail.com>
2021-06-09 21:29:50 +0300

    add bastion and db

---
+++ rds/variables.tf
@@ -0,0 +1,34 @@
+
+variable "allocated_storage" {
+  type        = number
+  default     = 10
+  description = "The default storage for the RDS instance"
+}
+
+variable "engine" {
+  default = "postgres"
+}
+
+variable "engine_version" {
+  default = "12" 
+}
+
+variable "instance_class" {
+  default = "db.t3.micro"
+}
+
+variable "database_name" {
+  default = "digger"
+}
+
+variable "database_username" {
+  default = "digger" 
+}
+
+variable "publicly_accessible" {
+  default = false
+}
+
+variable "vpc_security_group_ids" {
+  default = []
+}
