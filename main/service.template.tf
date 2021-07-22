d29832c8f049ec7ea1013d5b27c34ea95ff8b39f
motatoes <moe.habib9@gmail.com>
2021-07-22 23:05:33 +0300

    matcher

--- main/service.template.tf
+++ main/service.template.tf
@@ -26,6 +26,10 @@
     lb_protocol = "{{environment_config.lb_protocol}}"
     {% endif %}
 
+    {% if environment_config.health_check_matcher %}
+    health_check_matcher = "{{environment_config.health_check_matcher}}"
+    {% endif %}
+    
     # health_check_interval
     # health_check_timeout
     # health_check_matcher
