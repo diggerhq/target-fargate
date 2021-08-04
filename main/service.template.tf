2343279b0d125edc605a3d60e2c71f3c22c8b606
motatoes <moe.habib9@gmail.com>
2021-08-04 21:31:33 +0300

    support TCP services

--- main/service.template.tf
+++ main/service.template.tf
@@ -110,6 +110,45 @@
     value = ""
   }
 
+{% elif environment_config.tcp_service %}
+  
+  module "service-{{service_name}}" {
+    source = "../fargate-service-module"
+
+    ecs_cluster = aws_ecs_cluster.app
+    service_name = "{{service_name}}"
+    region = var.region
+    service_vpc = aws_vpc.vpc
+    service_security_groups = [aws_security_group.ecs_service_sg.id]
+    # image_tag_mutability
+    lb_subnet_a = aws_subnet.public_subnet_a
+    lb_subnet_b = aws_subnet.public_subnet_b
+    vpcCIDRblock = var.vpcCIDRblock
+    # lb_port
+    # lb_protocol
+    internal = false
+    # deregistration_delay
+    health_check = "{{health_check}}"
+    {% if environment_config.health_check_interval %}
+    health_check_interval = "{{environment_config.health_check_interval}}"
+    {% endif %}
+
+    # health_check_timeout
+    # health_check_matcher
+    # lb_access_logs_expiration_days
+    container_port = "{{container_port}}"
+    # replicas
+    container_name = "{{app_name}}-{{environment}}-{{service_name}}"
+    launch_type = "{{launch_type}}"
+    # ecs_autoscale_min_instances
+    # ecs_autoscale_max_instances
+    # lb_ssl_certificate_arn = "arn:aws:acm:eu-west-1:262499071169:certificate/411063e8-cd77-4498-921a-23adb15a1b9b"
+    default_backend_image = "quay.io/turner/turner-defaultbackend:0.2.0"
+    {% if task_cpu %}task_cpu = "{{task_cpu}}" {% endif %}
+    {% if task_memory %}task_memory = "{{task_memory}}" {% endif %}
+  }
+
+
 {% else %}
   module "service-{{service_name}}" {
     source = "../module-fargate-service-nolb"
