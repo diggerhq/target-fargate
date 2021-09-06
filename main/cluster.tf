d980d2de54d3a2abc29c61b6bb1b691d7651a587
motatoes <moe.habib9@gmail.com>
2021-09-06 12:41:16 +0300

    update cluster settings

--- main/cluster.tf
+++ main/cluster.tf
@@ -4,7 +4,7 @@ resource "aws_ecs_cluster" "app" {
   name = var.ecs_cluster_name
   setting {
     name  = "containerInsights"
-    value = "enabled"
+    value = "disabled"
   }
   tags = var.tags
 }
