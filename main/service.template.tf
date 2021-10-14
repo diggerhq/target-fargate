3ca5ced490fdda99c0f8c2e3f2152eb4464a1ce0
motatoes <moe.habib9@gmail.com>
2021-10-14 14:26:32 +0300

    make efs volume unique

--- main/service.template.tf
+++ main/service.template.tf
@@ -135,14 +135,14 @@
     {% if environment_config.include_efs_volume %}
       volumes = [
         {
-          name = "{{service_name}}_{{environment_config.efs_volume_name}}"
+          name = "${var.app}_${var.environment}_{{service_name}}_{{environment_config.efs_volume_name}}"
           file_system_id = module.{{service_name}}_efs_mount.fs_id
         }
       ]
 
       mountPoints = [{
         path = "{{environment_config.efs_volume_path}}"
-        volume = "{{service_name}}_{{environment_config.efs_volume_name}}"
+        volume = "${var.app}_${var.environment}_{{service_name}}_{{environment_config.efs_volume_name}}"
       }]
 
     {% endif %}
@@ -152,7 +152,7 @@
   {% if environment_config.include_efs_volume %}
     module "{{service_name}}_efs_mount" {
       source = "../efs_mount"
-      service_name = "{{service_name}}_{{environment_config.efs_volume_name}}"
+      service_name = "${var.app}_${var.environment}_{{service_name}}_{{environment_config.efs_volume_name}}"
       vpc_id = local.vpc.id
       subnet_a_id = aws_subnet.public_subnet_a.id
       subnet_b_id = aws_subnet.public_subnet_b.id
