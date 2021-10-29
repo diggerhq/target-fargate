ba737c387bdbf55795f99f5875649ce65a7354a5
motatoes <moe.habib9@gmail.com>
2021-10-29 20:37:32 +0300

    update engine

--- main/redis.tf
+++ main/redis.tf
@@ -32,7 +32,7 @@ resource "aws_elasticache_cluster" "dg_redis" {
   node_type            = var.redis_node_type
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  engine_version       = "6.0.5"
   port                 = local.redis_port
   security_group_ids   = [aws_security_group.dgredis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
