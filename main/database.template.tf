4d26b4d0dbf38ea779a8cce2a165b2a39fe6bf92
motatoes <moe.habib9@gmail.com>
2022-01-25 15:16:28 +0000

    support medusa

--- main/database.template.tf
+++ main/database.template.tf
@@ -1,94 +1,89 @@
-# additoinal argument to support self hosted
-{% if environment_config.needs_postgres is sameas True and not environment_config.rds_selfhosted  %}
 
+  resource "aws_db_subnet_group" "rds_private_subnet_group" {
+    name_prefix  = "rds_private_subnet_group"
+    subnet_ids   = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id ]
 
-    resource "aws_db_subnet_group" "rds_private_subnet_group" {
-      name_prefix  = "rds_private_subnet_group"
-      subnet_ids   = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id ]
-
-      tags = {
-        Name = "${var.app}-${var.environment}-rds-subnet-group"
-      }
-    }
-
-  resource "aws_security_group" "rds" {
-    name_prefix = "${var.app}-${var.environment}-rds-sg"
-    vpc_id = local.vpc.id
-    description = "RDS postgres servers (terraform-managed)"
-
-    # Only postgres in
-    ingress {
-      from_port = 5432
-      to_port = 5432
-      protocol = "tcp"
-      security_groups = [aws_security_group.ecs_service_sg.id, aws_security_group.bastion_sg.id]
-    }
-
-    # Allow all outbound traffic.
-    egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    tags = {
+      Name = "${var.app}-${var.environment}-rds-subnet-group"
     }
   }
 
-  module "app_rds" {
-    source = "../rds"
-    
-    {% if environment_config.rds_instance_type %}
-    instance_class = "{{environment_config.rds_instance_type}}"
-    {% endif %}
-    
-    {% if environment_config.rds_allocated_storage %}
-    allocated_storage = "{{environment_config.rds_allocated_storage}}"    
-    {% endif %}
+resource "aws_security_group" "rds" {
+  name_prefix = "${var.app}-${var.environment}-rds-sg"
+  vpc_id = local.vpc.id
+  description = "RDS postgres servers (terraform-managed)"
 
-    {% if environment_config.database_snapshot_identifier %}
-    snapshot_identifier = "{{environment_config.database_snapshot_identifier}}"
-    {% endif %}
-
-    {% if environment_config.database_iops %}
-    iops = "{{environment_config.database_iops}}"
-    {% endif %}
-
-    identifier_prefix = "${var.app}-${var.environment}"
-    db_subnet_group_name = aws_db_subnet_group.rds_private_subnet_group.name
-    vpc_security_group_ids = [aws_security_group.rds.id]
-    publicly_accessible = false
+  # Only postgres in
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = [aws_security_group.ecs_service_sg.id, aws_security_group.bastion_sg.id]
   }
 
-  locals {
-    database_address = module.app_rds.database_address
-    database_name = module.app_rds.database_name
-    database_username = module.app_rds.database_username
-    database_password = module.app_rds.database_password
-    database_port = module.app_rds.database_port
-    database_url = "postgres://${local.database_username}:${local.database_password}@${local.database_address}:${local.database_port}/${local.database_name}"
-    database_endpoint = "postgres://${local.database_username}:${local.database_password}@${local.database_address}:${local.database_port}/"
+  # Allow all outbound traffic.
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  resource "aws_ssm_parameter" "database_password" {
-    name = "${var.app}.${var.environment}.app_rds.database_password"
-    value = local.database_password
-    type = "SecureString"
-  }
+module "app_rds" {
+  source = "../rds"
+  
+  {% if environment_config.rds_instance_type %}
+  instance_class = "{{environment_config.rds_instance_type}}"
+  {% endif %}
+  
+  {% if environment_config.rds_allocated_storage %}
+  allocated_storage = "{{environment_config.rds_allocated_storage}}"    
+  {% endif %}
 
-  resource "aws_ssm_parameter" "database_url" {
-    name = "${var.app}.${var.environment}.app_rds.database_url"
-    value = local.database_url
-    type = "SecureString"
-  }
+  {% if environment_config.database_snapshot_identifier %}
+  snapshot_identifier = "{{environment_config.database_snapshot_identifier}}"
+  {% endif %}
 
-  resource "aws_ssm_parameter" "database_endpoint" {
-    name = "${var.app}.${var.environment}.app_rds.database_endpoint"
-    value = local.database_endpoint
-    type = "SecureString"
-  }
+  {% if environment_config.database_iops %}
+  iops = "{{environment_config.database_iops}}"
+  {% endif %}
 
-  output "DGVAR_DATABASE_URL" {
-    value = aws_ssm_parameter.database_url.arn
-  }
+  identifier_prefix = "${var.app}-${var.environment}"
+  db_subnet_group_name = aws_db_subnet_group.rds_private_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  publicly_accessible = false
+}
 
+locals {
+  database_address = module.app_rds.database_address
+  database_name = module.app_rds.database_name
+  database_username = module.app_rds.database_username
+  database_password = module.app_rds.database_password
+  database_port = module.app_rds.database_port
+  database_url = "postgres://${local.database_username}:${local.database_password}@${local.database_address}:${local.database_port}/${local.database_name}"
+  database_endpoint = "postgres://${local.database_username}:${local.database_password}@${local.database_address}:${local.database_port}/"
+}
 
-{% endif %}
+resource "aws_ssm_parameter" "database_password" {
+  name = "${var.app}.${var.environment}.app_rds.database_password"
+  value = local.database_password
+  type = "SecureString"
+}
+
+resource "aws_ssm_parameter" "database_url" {
+  name = "${var.app}.${var.environment}.app_rds.database_url"
+  value = local.database_url
+  type = "SecureString"
+}
+
+resource "aws_ssm_parameter" "database_endpoint" {
+  name = "${var.app}.${var.environment}.app_rds.database_endpoint"
+  value = local.database_endpoint
+  type = "SecureString"
+}
+
+output "DGVAR_DATABASE_URL" {
+  value = aws_ssm_parameter.database_url.arn
+}
+
---
+++ main/redis.template.tf
@@ -0,0 +1,51 @@
+
+locals {
+  redis_port = 6379
+  redis_url = "${aws_elasticache_cluster.dg_redis.cache_nodes[0].address}:${local.redis_port}"
+}
+
+resource "aws_security_group" "dgredis" {
+  name_prefix = "${var.app}-${var.environment}-dgdb-sg"
+  vpc_id = aws_vpc.vpc.id
+  description = "RDS postgres servers (terraform-managed)"
+
+  # Only postgres in
+  ingress {
+    from_port = 6379
+    to_port = 6379
+    protocol = "tcp"
+    security_groups = [aws_security_group.ecs_service_sg.id, aws_security_group.bastion_sg.id]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elasticache_cluster" "dg_redis" {
+  cluster_id           = "${var.app}-${var.environment}-dg-redis"
+  engine               = "redis"
+  node_type            = var.redis_node_type
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.x"
+  port                 = local.redis_port
+  security_group_ids   = [aws_security_group.dgredis.id]
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+}
+
+
+output "DGVAR_REDIS_URL" {
+  value = local.redis_url
+}
+
+
+# DATABASE_URL: postgres://postgres:postgres@postgres:5432/medusa-docker
+# REDIS_URL: redis://cache
+# NODE_ENV: development
+# JWT_SECRET: some_jwt_secret
+# COOKIE_SECRET: some_cookie_secret
