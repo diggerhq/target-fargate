82be9ad4c75ef8d8e909b8c8bf238d817b72e071
alexey <alexey@digger.dev>
2022-02-21 17:44:48 +0000

    fix jinja2 template

--- main/service.template.tf
+++ main/service.template.tf
@@ -290,7 +290,7 @@ module "monitoring-{{service_name}}" {
     service_name = "{{service_name}}"
     region = var.region
     service_vpc = local.vpc
-    scheduling_strategy = {{ "REPLICA" if standalone_task else "DAEMON" }}
+    scheduling_strategy = "{{ 'REPLICA' if standalone_task else 'DAEMON' }}"
     # image_tag_mutability
 
     {% if environment_config.use_subnets_cd %}
